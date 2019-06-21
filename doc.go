1b6a5a29860adf61958eb1f04782bacc5d8e5508
wangcheng <453451180@qq.com>
2019-06-21 17:29:19 +0800

    :fire: change package

--- doc.go
+++ doc.go
@@ -4,6 +4,6 @@ Set of wrappers around Ceph APIs.
 package rados
 
 import (
-	_ "github.com/ceph/go-ceph/rados"
-	_ "github.com/ceph/go-ceph/rbd"
+	_ "CephMonitorAPI/goceph/rados"
+	_ "CephMonitorAPI/goceph/rbd"
 )
