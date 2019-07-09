07c1d9d5450fdecc3f8ab0b4f256b12ce345fbca
wangcheng <453451180@qq.com>
2019-07-09 23:46:59 +0800

    :sparkles: 构建Image API路由

---
+++ api/handler/image_handler.go
@@ -0,0 +1,27 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+// 获取image信息
+func GetImageInfo(ctx *gin.Context) {
+
+}
+
+// 创建image
+func CreateImage(ctx *gin.Context) {
+
+}
+
+// 删除image
+func DeleteImage(ctx *gin.Context) {
+
+}
+
+// 获取image使用率
+func GetImageUsage(ctx *gin.Context) {
+
+}
+
+func UpdateImageSize(ctx *gin.Context) {
+
+}
