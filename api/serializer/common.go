71029aab1f45e8c775b67628610aef8fedd4e784
wangcheng <453451180@qq.com>
2019-07-09 18:04:43 +0800

    :bug: 修改json格式

--- api/serializer/common.go
+++ api/serializer/common.go
@@ -1,7 +1,7 @@
 package serializer
 
 type ResponseJSON struct {
-	Code int         `json: "code"`
-	Msg  string      `json: "msg"`
-	Data interface{} `json: "data"`
+	Code int         `json:"code"`
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
 }
