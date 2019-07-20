61b38ae293e7baf12b43afcca60d04fc700c158f
wangcheng <wangcheng@cechealth.cn>
2019-07-20 17:30:30 +0800

    修改路由

--- api/server/router.go
+++ api/server/router.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"CephMonitorAPI/api/handler"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,12 +16,15 @@ func NewRouter() *gin.Engine {
 		v1.POST("ping", handler.Ping)
 	}
 
+	poolApi := router.Group("api/v1/ceph/pool")
+	{
+		poolApi.POST("/:poolName", handler.CreatePool)
+		poolApi.DELETE("/:poolName", handler.DeletePool)
+	}
+
 	//rbd
 	rbdApi := router.Group("/api/v1/ceph/rbd")
 	{
-		rbdApi.POST("/pool/:poolName", handler.CreatePool)
-		rbdApi.DELETE("/pool/:poolName", handler.DeletePool)
-
 		rbdApi.POST("/:pool/image/:name/:size", handler.CreateImage)
 		rbdApi.DELETE("/:pool/image/:name", handler.DeleteImage)
 		rbdApi.GET("/:pool/image/:name/usage", handler.GetImageUsage)
